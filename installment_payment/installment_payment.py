aa17583b251e52f5074bf37a4b82a6e4d0a6b77c
JerryBluesnow <bbluesnow@126.com>
2018-01-22 11:00:21 +0800

    after merge

--- installment_payment/installment_payment.py
+++ installment_payment/installment_payment.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-=======
-
->>>>>>> ac3014a8a4f467669fd247ac4793405b9525b0fa
 import math
  
 # 定义函数
@@ -13,17 +9,10 @@ def installment_payment( monthly_interest_rate, payment_periods, repayment_month
     "repayment_month_index - 第几个月提前还款"
    
     #分母  (1+2+3+...+n-1), n>=2,等差数列求和公式 ( (repayment_month_index * (repayment_month_index - 1)) / 2 )
-<<<<<<< HEAD
     denominator = 2 * repayment_month_index * (payment_periods - repayment_month_index) + repayment_month_index * (repayment_month_index + 1)
 
     #分子 numerator： 
     numerator = 24 * (payment_periods * repayment_month_index * monthly_interest_rate + ((payment_periods - repayment_month_index) * 3 /(float)(100)) )
-=======
-    denominator = 25 * ((repayment_month_index * repayment_month_index) + (2 * payment_periods) - repayment_month_index)
-
-    #分子 numerator： 
-    numerator = 6 * ((100 * payment_periods * repayment_month_index * monthly_interest_rate) + (3 * (payment_periods - repayment_month_index)))
->>>>>>> ac3014a8a4f467669fd247ac4793405b9525b0fa
 
     yearly_interest_rate = numerator/(float)(denominator)
 
@@ -38,10 +27,7 @@ def installment_payment( monthly_interest_rate, payment_periods, repayment_month
 # 调用函数
 for repayment_month_index in range(1, 36 + 1):
     installment_payment(0.76/100, 36, repayment_month_index)
-<<<<<<< HEAD
 
 sequence = [3, 6, 12, 18, 24, 36]
 for index, payment_periods in enumerate(sequence):
     installment_payment(0.76/100, payment_periods, payment_periods)
-=======
->>>>>>> ac3014a8a4f467669fd247ac4793405b9525b0fa
