82c48253cee988c266a044fd9ec817960f8be86b
JerryBluesnow <bbluesnow@126.com>
2018-10-31 11:20:46 +0800

    add  spider/sipder_1.py

--- spider/sipder_1.py
+++ spider/sipder_1.py
@@ -32,6 +32,7 @@ else:
     infoencode = chardet.detect(html).get('encoding','utf-8')##通过第3方模块来自动提取网页的编码
     html = content.decode(infoencode,'ignore').encode(typeEncode)##先转换成unicode编码，然后转换系统编码输出
 
+#print html
+
+
 print html
-
-
