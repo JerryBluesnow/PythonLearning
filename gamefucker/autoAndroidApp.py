67496b382160d5bdcff4769c68aa9e917ecf5f29
JerryBluesnow <bbluesnow@126.com>
2018-12-19 08:53:47 +0800

    update with pyautogui

---
+++ gamefucker/autoAndroidApp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# [pywinauto简明教程](https://gaianote.github.io/2018/06/13/pywinauto%E7%AE%80%E6%98%8E%E6%95%99%E7%A8%8B/)
+# [pywinauto UserGuide](https://pywinauto.readthedocs.io/en/latest/getting_started.html)
+# [PyWinAuto入门指南](https://zhuanlan.zhihu.com/p/37283722)
+# [在中文windows下使用pywinauto进行窗口操作（一）](https://my.oschina.net/yangyanxing/blog/167042)
+import pyautogui
+from pywinauto.application import Application
+
+pyautogui.PAUSE = 1.5
+pyautogui.FAILSAFE = True
+
+__DEBUG__=True
+
+if __name__ == "__main__":
+    # get the size of the screen
+    if __DEBUG__ == True:
+        print "screen size:", pyautogui.size()
+
+    width, height = pyautogui.size()
+
+    #for i in range(1):
+    #      pyautogui.moveTo(300, 300, duration=0.25)
+    #      pyautogui.moveTo(400, 300, duration=0.25)
+    #      pyautogui.moveTo(400, 400, duration=0.25)
+    #      pyautogui.moveTo(300, 400, duration=0.25)
+#
+    print "Process Complete!......"
+    #pyautogui.typewrite("Process Complete!......", 0.25)
+
+    print Application.windows()
+    #app = Application().start("notepad.exe")
+    #app = Application().connect(class_name="Notepad")
+#
+    #app['Untitled - Notepad'].Edit.type_keys('test01')
+    #app['Untitled - Notepad'].menu_select("File->Save")
+#
+    #app['Save As'].Edit.type_keys('test.txt')
+    #app['Save As']['Save'].click()
+    ##app['确认另存为']['是'].click()
